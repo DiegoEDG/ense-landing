2fdf73f42ccbe8627f3b7ce0911523aa90d596a5
Digudev <1.5.dg.diego@gmail.com>
2023-05-22 21:32:32 -0600

    HOTFIX: Links added to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,25 @@
 import Image from 'next/image';
 import styles from './Navbar.module.css';
 import logo from './/../../public/EnseLogoPNG.png';
+import Link from 'next/link';
 
 const Navbar = () => {
 	return (
 		<nav className={styles.navbar}>
 			<Image width={187} height={64} src={logo} alt="logo" />
 			<ul className={styles.navlinks}>
-				<li>Home</li>
-				<li>Portafolio</li>
-				<li>Servicios</li>
-				<li>Nosotros</li>
+				<li>
+					<Link href="#">Home</Link>
+				</li>
+				<li>
+					<Link href="#">Portafolio</Link>
+				</li>
+				<li>
+					<Link href="#">Servicios</Link>
+				</li>
+				<li>
+					<Link href="#">Nosotros</Link>
+				</li>
 			</ul>
 		</nav>
 	);
