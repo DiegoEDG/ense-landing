1e958d7ecd836a96d83c2f58327e82214bd285ef
Digudev <1.5.dg.diego@gmail.com>
2023-05-22 20:35:52 -0600

    UI: Navbar Component implemented

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+import Image from 'next/image';
+import styles from './home.module.css';
+import logo from './/../../public/EnseLogoPNG.png';
+
 export default function Home() {
 	return (
-		<>
-			<h1>Ense Landing</h1>
-		</>
+		<div className={styles.main}>
+			<div className={styles.container}>
+				<nav className={styles.navbar}>
+					<Image width={187} height={64} src={logo} alt="logo" />
+					<ul className={styles.navlinks}>
+						<li>Home</li>
+						<li>Portafolio</li>
+						<li>Servicios</li>
+						<li>Nosotros</li>
+					</ul>
+				</nav>
+				<main>Main</main>
+				<footer>Footer</footer>
+			</div>
+		</div>
 	);
 }
