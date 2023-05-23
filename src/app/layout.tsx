261132314bb9ea3cf14cc63eeebc5b5f34898bcb
Digudev <1.5.dg.diego@gmail.com>
2023-05-22 20:34:27 -0600

    MISC: Font and metadata added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,22 @@
+import { Metadata } from 'next';
 import './globals.css';
+import { Montserrat } from 'next/font/google';
+
+const mon = Montserrat({
+	weight: ['400', '700'],
+	style: ['normal', 'italic'],
+	subsets: ['latin'],
+	display: 'swap'
+});
+
+export const metadata: Metadata = {
+	title: 'Ense Studio',
+	description: 'Digital Studio'
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<html lang="en">
+		<html lang="en" className={mon.className}>
 			<body>{children}</body>
 		</html>
 	);
