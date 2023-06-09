d62069895296a928dad902ff9aab8da9a8ba4266
Ricardo <tokioshawn25@gmail.com>
2023-06-08 20:01:24 -0600

    UI: Images Fixed

--- src/app/studyCase.js
+++ src/app/studyCase.js
@@ -1,5 +1,5 @@
 import Hoobank from '../../public/hoobank.webp';
-import Geritch from '../../public/gerich.webp';
+import Geritch from '../../public/geritch.webp';
 
 const studyCase = [
 	{
@@ -8,7 +8,7 @@ const studyCase = [
 		title: 'Hoobank',
 		paragraph:
 			'En colaboración con una startup fintech mexicana que ofrece nuevos métodos de pago con recompensas, diseñamos una interfaz moderna y limpia que reflejara la simplicidad y elegancia de su producto. Nuestro objetivo era crear una imagen de alto impacto que transmitiera confianza y profesionalismo a sus usuarios.',
-		href: '#'
+		href: 'https://diegoedg.github.io/Hoobank-landingPage/'
 	},
 
 	{
@@ -17,7 +17,7 @@ const studyCase = [
 		title: 'Gericht',
 		paragraph:
 			'Diseño de marca y sitio web para un restaurante local con una propuesta gastronómica innovadora. Nos enfocamos en crear una experiencia de usuario atractiva y fácil de usar en el sitio web, con una estética, moderna y atractiva que refleja la personalidad de la marca y la calidad de su comida. Además, diseñamos elementos de marca consistentes, como el logotipo, la paleta de colores y las imágenes de los platillos, para que el restaurante tenga una presencia coherente y memorable en línea y fuera de ella.',
-		href: '#'
+		href: 'https://gerichsite.netlify.app/'
 	}
 ];
 export default studyCase;
